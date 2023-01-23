e335c0814c18ee4f87ec08e03eff1a9621053bdc
Daniel Lopes Petersen <suporte@superahost.com>
2023-01-23 15:51:25 -0400

    model corrigida

--- Models/LivrosBiblia.cs
+++ Models/LivrosBiblia.cs
@@ -1,60 +1,64 @@
 ﻿namespace api_biblia.Models
 {
-    public class LivrosBiblia
+    public class Abbrev
     {
-        public class Book
+        public Abbrev(string pt, string en)
         {
-            public Book(string name, string author, string group, string version)
-            {
-                Name = name;
-                Author = author;
-                Group = group;
-                Version = version;
-            }
+            Pt = pt;
+            En = en;
+        }
 
-            public class Abrev
-            {
-                public Abrev(string pt, string en)
-                {
-                    Pt = pt;
-                    En = en;
-                }
-
-                public string Pt { get; set; }
-                public string En { get; set;}
-            }
+        public string Pt { get; set; }
+        public string En { get; set; }
+    }
+    public class Book
+    {
+        public Book(Abbrev abbrev, string name, string author, string group, string version)
+        {
+            Abbrev = abbrev;
+            Name = name;
+            Author = author;
+            Group = group;
+            Version = version;
+        }
+        public Abbrev Abbrev { get; set; }
         public string Name { get; set; }
         public string Author { get; set; }
         public string Group { get; set; }
         public string Version { get; set; }
-            }
 
-        public class Chapter
+    }
+
+    public class Chapter
+    {
+        public Chapter(int number, int verses)
         {
-            public Chapter(int number, int verses)
-            {
-                Number = number;
-                Verses = verses;
-            }
-
-            public int Number { get; set; }
-            public int Verses { get; set; }
+            Number = number;
+            Verses = verses;
         }
 
-        public class Verses
-        {
-            public Verses(int number, string text)
-            {
-                Number = number;
-                Text = text;
-            }
+        public int Number { get; set; }
+        public int Verses { get; set; }
+    }
 
-            public int Number { get; set; }
-            public string Text { get; set; }
+    public class Verse
+    {
+        public Verse(int number, string text)
+        {
+            Number = number;
+            Text = text;
         }
 
+        public int Number { get; set; }
+        public string Text { get; set; }
+    }
 
-       // public List<Capitulo> Capitulo { get; set; }
-    
+    public class LivrosBiblia
+    {
+
+        public Book Book { get; set; }
+        public Chapter Chapter { get; set; }
+        public IEnumerable<Verse> Verses { get; set; }
+
     }
 }
