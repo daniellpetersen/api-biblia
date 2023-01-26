788263aaf945903434db38b8a800698a1b07deb3
DEVOPS <DEVOPS@DESKTOP-71KA40J>
2023-01-25 22:44:17 -0400

    Todos os Gets prontos

--- Models/LivrosBiblia.cs
+++ Models/LivrosBiblia.cs
@@ -1,60 +1,57 @@
-﻿namespace api_biblia.Models
+﻿
+namespace api_biblia.Models;
+
+public class Book
 {
-    public class LivrosBiblia
+    public Book(string abbrev, string name, string author, string group, string version)
     {
-        public class Book
-        {
-            public Book(string name, string author, string group, string version)
-            {
-                Name = name;
-                Author = author;
-                Group = group;
-                Version = version;
-            }
-
-            public class Abrev
-            {
-                public Abrev(string pt, string en)
-                {
-                    Pt = pt;
-                    En = en;
-                }
-
-                public string Pt { get; set; }
-                public string En { get; set;}
-            }
-        public string Name { get; set; }
-        public string Author { get; set; }
-        public string Group { get; set; }
-        public string Version { get; set; }
-            }
-
-        public class Chapter
-        {
-            public Chapter(int number, int verses)
-            {
-                Number = number;
-                Verses = verses;
-            }
-
-            public int Number { get; set; }
-            public int Verses { get; set; }
-        }
-
-        public class Verses
-        {
-            public Verses(int number, string text)
-            {
-                Number = number;
-                Text = text;
-            }
-
-            public int Number { get; set; }
-            public string Text { get; set; }
-        }
-
-
-       // public List<Capitulo> Capitulo { get; set; }
-    
+        Abbrev = abbrev;
+        Name = name;
+        Author = author;
+        Group = group;
+        Version = version;
     }
+
+    public string Abbrev { get; set; }
+    public string Name { get; set; }
+    public string Author { get; set; }
+    public string Group { get; set; }
+    public string Version { get; set; }
 }
+
+
+public class Chapter
+    {
+        public Chapter(int number, int verses)
+        {
+            Number = number;
+            Verses = verses;
+        }
+
+        public int Number { get; set; }
+        public int Verses { get; set; }
+    }
+
+
+    public class Verses
+    {
+        public Verses(int number, string text)
+        {
+            Number = number;
+            Text = text;
+        }
+
+        public int Number { get; set; }
+        public string Text { get; set; }
+    }
+
+
+public class LivrosBiblia
+{
+    public Book books { get; set; }
+    public Chapter chapters { get; set; }
+    public Verses verses { get; set; }
+
+
+    }
+
