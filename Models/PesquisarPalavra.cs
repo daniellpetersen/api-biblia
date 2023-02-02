fe9136b083098405c37a3c175449041d32e73b51
DEVOPS <DEVOPS@DESKTOP-71KA40J>
2023-02-01 23:05:10 -0400

    Adicionado Post de Buscar Palavra

--- Models/PesquisarPalavra.cs
+++ Models/PesquisarPalavra.cs
@@ -4,16 +4,24 @@ namespace api_biblia.Models
 {
     public class Verses
     {
-        public Book? Book { get; set; }
-        public string? Chapters { get; set; }
+        public Verses(Livros? book, string? chapter, int? number, string? text)
+        {
+            Livros = book;
+            Capitulo = chapter;
+            Numero = number;
+            Texto = text;
+        }
+        public Livros? Livros { get; }
 
-        public string? Testament { get; set; }
+        [JsonProperty("chapter")]
+        public string? Capitulo { get; set; }
 
+        [JsonProperty("number")]
+        public int? Numero { get; set; }
 
-        public Abbrev? Abreviatura { get; set; }
+        [JsonProperty("text")]
+        public string? Texto { get; set; }
     }
-
-
     public class PesquisarPalavra
     {
         [JsonProperty("occurrence")]
@@ -23,7 +31,6 @@ namespace api_biblia.Models
         public string? Versão { get; set; }
 
        public IEnumerable<Verses>? Verses { get; set; }
-        
 
     }
 }
